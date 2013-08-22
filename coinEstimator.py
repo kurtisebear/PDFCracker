3c3a4b0884b98a21dfdb011d2900dda6d7963590
kurtisebear <kurtis@kurtisebear.com>
2013-08-22 14:33:38 +0100

    added inputs for weights

--- coinEstimator.py
+++ coinEstimator.py
@@ -11,3 +11,22 @@ onepound = 9.5
 
 
 
+# ask for input
+
+amountone = input("Please enter the weight of your 1p\'s in grams?  ")
+amounttwo = input("Please enter the weight of your 2p\'s in grams?  ")
+amountfive = input("Please enter the weight of your 5p\'s in grams?  ")
+amountten = input("Please enter the weight of your 10p\'s in grams?  ")
+amounttwenty = input("Please enter the weight of your 20p\'s in grams?  ")
+amountfifty = input("Please enter the weight of your 50p\'s in grams?  ")
+amountonepound = input("Please enter the weight of your 1 pound's in grams?  ")
+
+
+
+
+# return estimate of coin worth
+
+
+
+#return estimate of total amount of coins
+
