b213c6524c2b784b8dd9710e90ef22bc98ca6cc3
kurtisebear <kurtis@kurtisebear.com>
2013-08-22 16:11:21 +0100

    add README

---
+++ README.md
@@ -0,0 +1,6 @@
+I am following the projects on 
+
+http://www.reddit.com/r/beginnerprojects
+
+and completing them using python
+
--- coinEstimator.py
+++ coinEstimator.py
@@ -24,9 +24,35 @@ amountonepound = input("Please enter the weight of your 1 pound's in grams?  ")
 
 
 
+
 # return estimate of coin worth
 
 
 
 #return estimate of total amount of coins
+#amount of coins will be varibles / total weight
+
+print "-" * 10
+print "Total Amount Of Coins"
+
+print1 = "you have " + str(round(amountone/onep)) + " one pence coins"
+print2 = "you have " + str(round(amounttwo/twop)) + " two pence coins"
+print3 = "you have " + str(round(amountfive/fivep)) + " five pence coins"
+print4 = "you have " + str(round(amountten/tenp)) + " ten pence coins"
+print5 = "you have " + str(round(amounttwenty/twentyp)) + " twenty pence coins"
+print6 = "you have " + str(round(amountfifty/fiftyp)) + " fifty pence coins"
+print7 = "you have " + str(round(amountonepound/onepound)) + " one pound coins"
+
+
+
+
+
+print print1
+print print2
+print print3
+print print4
+print print5
+print print6
+print print7
+
 
